dcd5c76b23bf3b2aff662f5ba7c8fad971effb34
MarcosMiotto711 <129965433+MarcosMiotto711@users.noreply.github.com>
2023-06-03 22:16:28 -0300

    Resultado final

--- Fase3.py
+++ Fase3.py
@@ -1,6 +1,132 @@
 import tkinter
 from tkinter import ttk
 import oracledb
+import numpy as np
+
+
+def letra_num(matriz):
+    nova_matriz = []
+    for i in matriz:
+        a = ord(i[0]) - ord('A') + 1
+        b = ord(i[1]) - ord('A') + 1
+        if a == 26:
+            a = 0
+        if b == 26:
+            b = 0
+        nova_matriz.append([a, b])
+    return nova_matriz
+
+
+def num_letra(matriz):
+    matriz = np.array(matriz).T
+    nova_matriz = []
+    for i in matriz:
+        a = chr(i[0] + ord('A') - 1)
+        b = chr(i[1] + ord('A') - 1)
+        if a == '@':
+            a = 'Z'
+        if b == '@':
+            b = 'Z'
+        nova_matriz.append([a, b])
+    return nova_matriz
+
+
+#   CRIPTOGRAFIA:
+def criptografia(nome):
+    nome = nome.upper()
+    blanck_ind = []
+    x = 0
+    for i in nome:
+        if i == ' ':
+            blanck_ind.append(x)
+        x += 1
+    nome = nome.replace(' ', '')
+    if len(nome) % 2 != 0:
+        nome = nome + nome[len(nome) - 1]
+    matriz_nome = []
+    a = np.array([[2, 1], [5, 3]])
+    for i in range(len(nome)):
+        if i % 2 == 0:
+            pass
+        else:
+            matriz_nome.append([nome[i - 1], nome[i]])
+    c = np.array(letra_num(matriz_nome)).T
+    p = np.dot(a, c)
+    matriz_cod = []
+    for i in p:
+        lista = []
+        for j in i:
+            if j >= 26:
+                lista.append(j % 26)
+            else:
+                lista.append(j)
+        matriz_cod.append(lista)
+    cripto = num_letra(matriz_cod)
+    mensagem = ''
+    for i in cripto:
+        mensagem = mensagem + i[0] + i[1]
+    for i in blanck_ind:
+        mensagem = mensagem[0:i] + ' ' + mensagem[i:]
+    cursor = conexao.cursor()
+    cursor.execute(f"INSERT INTO CLASSIFICACAO (VALOR) VALUES ('{mensagem}')")
+    conexao.commit()
+    cursor.close()
+
+
+def descriptografia():
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM CLASSIFICACAO")
+    resultados = cursor.fetchall()
+    ids = []
+    for i in resultados:
+        ids.append(i[0])
+    id = max(ids)
+    nome = ''
+    for i in resultados:
+        if i[0] == id:
+            nome = i[1]
+    blanck_ind = []
+    x = 0
+    for i in nome:
+        if i == ' ':
+            blanck_ind.append(x)
+        x += 1
+    nome = nome.replace(' ', '')
+    matriz_nome = []
+    for i in range(len(nome)):
+        if i % 2 == 0:
+            pass
+        else:
+            matriz_nome.append([nome[i - 1], nome[i]])
+    c = np.array(letra_num(matriz_nome)).T
+    a = np.array([[3, -1], [-5, 2]])
+    p = np.dot(a, c)
+    matriz_cod = []
+    for i in p:
+        lista = []
+        for j in i:
+            if j < 0:
+                valor = j * -1
+                valor = (int(valor / 26) * 26) + 26
+                valor = valor + j
+                lista.append(valor)
+            elif j > 26:
+                valor = j % 26
+                lista.append(valor)
+            else:
+                lista.append(j)
+        matriz_cod.append(lista)
+    cripto = num_letra(matriz_cod)
+    mensagem = ''
+    for i in cripto:
+        mensagem = mensagem + i[0] + i[1]
+    for i in blanck_ind:
+        mensagem = mensagem[0:i] + ' ' + mensagem[i:]
+    if mensagem[len(mensagem) - 1] == mensagem[len(mensagem) - 2]:
+        mensagem = mensagem[0:len(mensagem) - 1]
+    mensagem = mensagem.lower()
+    cursor.close()
+    return mensagem
 
 
 #   FUNÇÃO PARA ADIÇÃO DE UMA AMOSTRA
@@ -309,9 +435,106 @@ def excluir_win():
 #   Função da classificação da qualidade do ar segundo as amostras dadas
 def classificar_win():
     classificar_window = tkinter.Toplevel()
-    classificar_window.title("Classificar ")
+    classificar_window.title("Classificar amostra")
+    classificar_window.resizable(height=False, width=False)
     classificar_window.iconbitmap('icone.ico')
-    classificar_window.geometry(f'400x250+{largwindow - 200}+{altwindow - 125}')
+    classificar_window.geometry(f'500x270+{largwindow - 250}+{altwindow - 125}')
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM AMOSTRAS")
+    amostras = cursor.fetchall()
+    if amostras == []:
+        tkinter.Label(classificar_window, text='Nenhuma amostra cadastrada!', font='Arial 20 bold').place(x=50, y=90)
+        tkinter.Label(classificar_window, text='', background='black').place(x=157, y=204, width=185, height=49)
+        retornar = tkinter.Button(classificar_window, text='Voltar ao Menu', font=fonte, fg='black',
+                                  bd=0, command=classificar_window.destroy)
+        retornar.pack(side='bottom', pady=20)
+        return
+    mp10 = 0
+    mp25 = 0
+    o3 = 0
+    co = 0
+    no2 = 0
+    so2 = 0
+    for i in amostras:
+        mp10 += i[1]
+        mp25 += i[2]
+        o3 += i[3]
+        co += i[4]
+        no2 += i[5]
+        so2 += i[6]
+    x = len(amostras)
+    mp10 = mp10 / x
+    mp25 = mp25 / x
+    o3 = o3 / x
+    co = co / x
+    no2 = no2 / x
+    so2 = so2 / x
+    if mp10 > 250 or mp25 > 125 or o3 > 200 or co > 15 or no2 > 1130 or so2 > 800:
+        classificacao = 'pessima'
+    elif mp10 > 150 or mp25 > 75 or o3 > 160 or co > 13 or no2 > 320 or so2 > 365:
+        classificacao = 'muito ruim'
+    elif mp10 > 100 or mp25 > 50 or o3 > 130 or co > 11 or no2 > 240 or so2 > 40:
+        classificacao = 'ruim'
+    elif mp10 > 50 or mp25 > 25 or o3 > 100 or co > 9 or no2 > 200 or so2 > 20:
+        classificacao = 'moderada'
+    else:
+        classificacao = 'boa'
+    criptografia(classificacao)
+    resultado = descriptografia()
+    cursor.close()
+    cor = ''
+    if resultado == 'boa':
+        cor = '#57b563'
+        tkinter.Label(classificar_window, text='A qualidade do ar é boa!', font='Arial 20 bold',
+                      background='#57b563').place(x=87, y=90)
+        classificar_window.configure(background='#57b563')
+    if resultado == 'moderada':
+        cor = '#f0ed46'
+        tkinter.Label(classificar_window, text='A qualidade do ar é moderada!', font='Arial 20 bold',
+                      background='#f0ed46').place(x=45, y=35)
+        tkinter.Label(classificar_window, text='''Com a qualidade do ar neste estado, pessoas de
+grupos sensíveis (crianças, idosos e pessoas com
+doenças respiratórias e cardíacas) podem apresentar
+sintomas como tosse seca e cansaço. A população, 
+em geral, não é afetada.''', font='Arial 14 ', background='#f0ed46').place(x=15, y=70)
+        classificar_window.configure(background='#f0ed46')
+    if resultado == 'ruim':
+        cor = '#ee8c36'
+        tkinter.Label(classificar_window, text='A qualidade do ar é ruim!', font='Arial 20 bold',
+                      background='#ee8c36').place(x=80, y=25)
+        tkinter.Label(classificar_window, text='''Com a qualidade do ar neste estado, toda a população
+pode apresentar sintomas como tosse seca, cansaço, 
+ardor nos olhos, nariz e garganta. Pessoas de grupos
+sensíveis (crianças, idosos e pessoas com doenças
+respiratórias e cardíacas) podem apresentar efeitos
+mais sérios na saúde.''', font='Arial 14', background='#ee8c36').place(x=15, y=60)
+        classificar_window.configure(background='#ee8c36')
+    if resultado == 'muito ruim':
+        cor = '#d63636'
+        tkinter.Label(classificar_window, text='A qualidade do ar é muito ruim!', font='Arial 20 bold',
+                      background='#d63636').place(x=38, y=10)
+        tkinter.Label(classificar_window, text='''Com a qualidade do ar neste estado, toda a população
+pode apresentar agravamento dos sintomas como tosse
+seca, cansaço, ardor nos olhos, nariz e garganta e
+ainda falta de ar e respiração ofegante. Pode ocasionar
+também em efeitos ainda mais graves à saúde de grupos
+sensíveis (crianças, idosos e pessoas com doenças
+respiratórias e cardíacas)''', font='Arial 14', background='#d63636').place(x=5, y=43)
+        classificar_window.configure(background='#d63636')
+    if resultado == 'pessima':
+        cor = '#fd1616'
+        tkinter.Label(classificar_window, text='A qualidade do ar é péssima!', font='Arial 20 bold',
+                      background='#fd1616').place(x=56, y=35)
+        tkinter.Label(classificar_window, text='''Com a qualidade do ar neste estado, toda a população
+pode apresentar sérios riscos de manifestações de
+doenças respiratórias e cardiovasculares. Como
+consequência, ocorre o aumento de mortes prematuras
+em pessoas de grupos sensíveis.''', font='Arial 14', background='#fd1616').place(x=10, y=70)
+        classificar_window.configure(background='#fd1616')
+    tkinter.Label(classificar_window, text='', background='black').place(x=157, y=204, width=185, height=49)
+    retornar = tkinter.Button(classificar_window, text='Voltar ao Menu', font=fonte, fg='black', bg=cor,
+                              bd=0, command=classificar_window.destroy)
+    retornar.pack(side='bottom', pady=20)
 
 
 #   ESTABELECENDO CONEXÃO COM O BANCO DE DADOS
--- teste.py
+++ teste.py
@@ -1,1 +1,54 @@
-print(len(['ola', 'tudo bem?']))
+import tkinter
+
+classificar_window = tkinter.Tk()
+classificar_window.title("Classificar amostra")
+classificar_window.resizable(height=False, width=False)
+classificar_window.iconbitmap('icone.ico')
+classificar_window.geometry(f'500x230+500+200')
+resultado = 'pessima'
+if resultado == 'boa':
+    tkinter.Label(classificar_window, text='A qualidade do ar é boa!', font='Arial 20 bold',
+                  background='#57b563').place(x=87, y=90)
+    classificar_window.configure(background='#57b563')
+if resultado == 'moderada':
+    tkinter.Label(classificar_window, text='A qualidade do ar é moderada!', font='Arial 20 bold',
+                  background='#f0ed46').place(x=45, y=35)
+    tkinter.Label(classificar_window, text='''Com a qualidade do ar neste estado, pessoas de
+grupos sensíveis (crianças, idosos e pessoas com
+doenças respiratórias e cardíacas) podem apresentar
+sintomas como tosse seca e cansaço. A população, 
+em geral, não é afetada.''', font='Arial 14 ', background='#f0ed46').place(x=15, y=70)
+    classificar_window.configure(background='#f0ed46')
+if resultado == 'ruim':
+    tkinter.Label(classificar_window, text='A qualidade do ar é ruim!', font='Arial 20 bold',
+                  background='#ee8c36').place(x=80, y=25)
+    tkinter.Label(classificar_window, text='''Com a qualidade do ar neste estado, toda a população
+pode apresentar sintomas como tosse seca, cansaço, 
+ardor nos olhos, nariz e garganta. Pessoas de grupos
+sensíveis (crianças, idosos e pessoas com doenças
+respiratórias e cardíacas) podem apresentar efeitos
+mais sérios na saúde.''', font='Arial 14', background='#ee8c36').place(x=15, y=60)
+    classificar_window.configure(background='#ee8c36')
+if resultado == 'muito ruim':
+    tkinter.Label(classificar_window, text='A qualidade do ar é muito ruim!', font='Arial 20 bold',
+                  background='#d63636').place(x=38, y=15)
+    tkinter.Label(classificar_window, text='''Com a qualidade do ar neste estado, toda a população
+pode apresentar agravamento dos sintomas como tosse
+seca, cansaço, ardor nos olhos, nariz e garganta e
+ainda falta de ar e respiração ofegante. Pode ocasionar
+também em efeitos ainda mais graves à saúde de grupos
+sensíveis (crianças, idosos e pessoas com doenças
+respiratórias e cardíacas)
+''', font='Arial 14', background='#d63636').place(x=5, y=50)
+    classificar_window.configure(background='#d63636')
+if resultado == 'pessima':
+    tkinter.Label(classificar_window, text='A qualidade do ar é péssima!', font='Arial 20 bold',
+                  background='#fd1616').place(x=56, y=35)
+    tkinter.Label(classificar_window, text='''Com a qualidade do ar neste estado, toda a população
+pode apresentar sérios riscos de manifestações de
+doenças respiratórias e cardiovasculares. Como
+consequência, ocorre o aumento de mortes prematuras
+em pessoas de grupos sensíveis.''', font='Arial 14', background='#fd1616').place(x=10, y=70)
+    classificar_window.configure(background='#fd1616')
+
+classificar_window.mainloop()
